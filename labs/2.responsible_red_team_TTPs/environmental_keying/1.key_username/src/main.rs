f63aedc459db85eff387ac9a0dc89821b722e590
husky <huskyhacks.mk@gmail.com>
2022-10-12 09:29:44 -0400

    git ignore and username key

--- labs/2.responsible_red_team_TTPs/environmental_keying/1.key_username/src/main.rs
+++ labs/2.responsible_red_team_TTPs/environmental_keying/1.key_username/src/main.rs
@@ -1,3 +1,45 @@
+extern crate whoami;
+
+use std::process::exit;
+use whoami::{username};
+
+
+fn payload() {
+    // Placeholder for any kind of payload that we want to run
+    println!("[+] Boom!");
+}
+
+fn check_env_keys() -> bool {
+    println!("Checking keys...");
+
+    // For this demo, assume a static value for the username
+    let key_username: String = "husky".to_string();
+    // This value can be hard coded in the code here or passed in via arguments.
+
+    let current_username = username().to_lowercase();
+    println!("[+] Username: {}", current_username);
+    println!("[+] Looking for: {}", key_username);
+    
+    // Evaluate the keying criteria, return true or false
+    current_username == key_username    
+}
+
+fn does_not_match() {
+    println!("[-] No match, sorry :(");
+    println!("[-] Exiting...");
+    exit(0);
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    //Check the env keys
+    let key_check_result = check_env_keys();
+
+    //If they match, trigger the payload
+    //If not, exit
+    if !key_check_result {
+        does_not_match();
+    } else {
+        payload()
+    }
 }
